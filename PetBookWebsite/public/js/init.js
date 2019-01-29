d0cb1a2d27a231359c4f51581bad5364364b59c6
Emz27 <emmanuelbenitez1996@gmail.com>
2019-01-29 15:34:39 +0800

    session

--- PetBookWebsite/public/js/init.js
+++ PetBookWebsite/public/js/init.js
@@ -1,34 +1,55 @@
 // initialize firebase
-(async function(){
-  try {
-    await firebase.initializeApp({
-    apiKey: "AIzaSyCHUtKxC3ywtoHHj1QVuZVG6FliyR-u4pw",
-    authDomain: "petbook-d35a1.firebaseapp.com",
-    databaseURL: "https://petbook-d35a1.firebaseio.com",
-    projectId: "petbook-d35a1",
-    storageBucket: "petbook-d35a1.appspot.com",
-    messagingSenderId: "397515456686"
-  })
-  } catch (err) {
-      // we skip the "already exists" message which is
-      // not an actual error when we're hot-reloading
-      if (!/already exists/.test(err.message)) {
-      console.error('Firebase initialization error', err.stack)
-      }
-  }
-  var db = firebase.firestore();
+try {
+firebase.initializeApp({
+  apiKey: "AIzaSyCHUtKxC3ywtoHHj1QVuZVG6FliyR-u4pw",
+  authDomain: "petbook-d35a1.firebaseapp.com",
+  databaseURL: "https://petbook-d35a1.firebaseio.com",
+  projectId: "petbook-d35a1",
+  storageBucket: "petbook-d35a1.appspot.com",
+  messagingSenderId: "397515456686"
+})
+} catch (err) {
+    // we skip the "already exists" message which is
+    // not an actual error when we're hot-reloading
+    if (!/already exists/.test(err.message)) {
+    console.error('Firebase initialization error', err.stack)
+    }
+}
+var db = firebase.firestore();
 
-  
 
-  db.settings({
+
+db.settings({
   timestampsInSnapshots: true
-  });
+});
+(async function(){
+  
 })();
 // session checker
 (async function(){
-  var user = JSON.parse(sessionStorage.getItem("user"));
-  if(user){
-
+  var userSession = JSON.parse(sessionStorage.getItem("user"));
+  console.log("userSession: ", userSession)
+  if(userSession){
+    try{
+      var type = userSession.type;
+      
+      var userResult = await firebase.firestore().collection("users").where("username", "==", userSession.username).get()
+      if(userResult.docs.length == 0){
+        sessionStorage.clear();
+        window.location.href = "404.html";
+      }
+      else if( !window.location.pathname.includes("login.html") 
+          && !window.location.pathname.includes("register.html")
+          && !window.location.pathname.includes("/"+type+"/")
+      ){
+        window.location.href = "404.html";
+      }
+    }
+    catch(e){
+      console.log(e.message);
+      window.location.href = "404.html";
+    }
+    
   }
   else{
     if(!window.location.pathname.includes("login.html") && !window.location.pathname.includes("register.html")){
